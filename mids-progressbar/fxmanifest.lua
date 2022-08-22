cc28b1a401bbe77afadf3dd6b8297764fdac8803
Midsetts <100185331+Midsetts@users.noreply.github.com>
2022-08-22 19:03:28 -0400

    Add files via upload

---
+++ mids-progressbar/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+
+ui_page('html/index.html') 
+
+client_scripts {
+    'client/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+
+    'html/css/bootstrap.min.css',
+    'html/js/jquery.min.js',
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick'
+}
+
+
+
+client_script '@crp-mapassets/xDxDxDxDxD.lua'
+
+client_script '@crp-mapassets/xDxDxDxDxD.lua'
